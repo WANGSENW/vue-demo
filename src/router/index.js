404596d3039107d97b23d684a18b1aeb352e48a9
wangsen <15841621107m0@sina.cn>
2021-01-18 16:39:05 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,10 @@ const routes = [
   },
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
 
 export default router
