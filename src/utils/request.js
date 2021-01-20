53bbf52a9e50962c92449f0aa108458d35f09056
wangsen <15841621107m0@sina.cn>
2021-01-20 11:14:10 +0800

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { Loading, Message } from 'element-ui';
 export const request = ({ url, method, data }) => {
   console.log(url, method, data)
   return new Promise((resolve, reject) => {
-   axios.get(url,data).then((result) => {
+   axios.get('http://81.70.229.157:3000/api/user/listUser',data).then((result) => {
      console.log(result)
    }).catch((err) => {
      
